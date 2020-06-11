a9937703ed4f9d39ecfae7e10b1bc4f8e1b3c60e
vitashevdotdata <vitalii.shevchuk@dotdata.com>
2020-06-11 09:08:29 -0700

    stack learn

--- data-structure/cpp/array.dSYM/Contents/Resources/DWARF/stack.cpp
+++ data-structure/cpp/array.dSYM/Contents/Resources/DWARF/stack.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
+template<class T>
+class Stack {
+   public:
+      struct Element {
+         T value;
+         Element *prev;
+         Element *next;
+      } element;
+      Element *tail;
+      Element *head;
+      Stack(T item){
+         // struct Element element; 
+         element.value = item;
+         element.prev = NULL;
+         element.next = NULL;
+         // *tail = &element;
+         // *head = &element;
+      };
+      T getTail(){
+         return element.value;
+      };
+};
+
 int main () {
    double* pvalue  = NULL; // Pointer initialized with null
    pvalue  = new double;   // Request memory for the variable
@@ -9,6 +33,11 @@ int main () {
    cout << "Value of pvalue : " << *pvalue << endl;
 
    delete pvalue;         // free up the memory.
+   int n  = 3;
+   Stack<int> stack(n);
+
+
+   cout << "Value of pvalue : " << stack.getTail()<< endl;
 
    return 0;
 }
