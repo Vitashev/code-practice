355760c61642c6479c2df85ef9db7536fd716d0c
Macbook <macbook@Macbook-MacBook-Pro.local>
2020-06-13 11:55:47 -0700

    merged sorted array completed

--- challanges/mergeSortedArrays.cpp
+++ challanges/mergeSortedArrays.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int *mergeSortedArrays(int ar1[], int ar2[]){
-    int len1 = sizeof(ar1)/sizeof(ar1[0]);
-    int len2 = sizeof(ar2)/sizeof(ar2[0]);
-    int size =  len1 + len2;
+int* mergeSortedArrays(int* ar1, int* ar2, int size1, int size2){
+ 
+    int size = size1 + size2;
     int *ar = new int[size];
 
-    for(unsigned int i = 0, j = 0, k = 0; i < len1 || j < len2; k++){
-        if(ar1[i] < ar1[j]){
-           ar[k] = ar1[i];
-           i++;
-        } else {
-           ar[k] = ar1[j];
-          j++;
-        }
-
+    for(unsigned int i = 0, j = 0, k = 0; k < size; k++){
+       if((i < size1 && j >= size2) || (ar1[i] < ar2[j])){
+                ar[k] = ar1[i];
+                i++;
+            } else {
+                ar[k] = ar2[j];
+                j++;
+            }
     }
-
-
 
   return  ar;
 }
@@ -26,11 +22,17 @@ int *mergeSortedArrays(int ar1[], int ar2[]){
 
 
 int main (){
-    int ar1[] = {0,3,4,31};
-    int ar2[] = {4,5,30};
+    int ar1[] = {0, 3, 4};
+    int ar2[] = {4, 5, 30, 31,44};
 
+    int size1 = sizeof(ar1)/sizeof(*ar1);
+    int size2 = sizeof(ar2)/sizeof(*ar2);
+    int size = size1 + size2;
+    int *mergedArray = mergeSortedArrays(ar1, ar2, size1, size2);
 
+    for(int i = 0; i < size; i++){
+        cout<<mergedArray[i]<<" ";
+    }
 
-    cout<<"hello ";
     return 0;
 }
